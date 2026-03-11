7eb0f5fd6a311e100fd9523f8f2032994d58cc8a
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2026-03-11 19:30:00 +0700

    1

--- sc.lua
+++ sc.lua
@@ -191,7 +191,7 @@ _G.FishItConfig = _G.FishItConfig or {
             ["Fisherman Island"] = {"Starter Rod"},
             ["Kohana Volcano"] = {"Grass Rod", "Midnight Rod"},
             ["Esoteric Depths"] = {"Steampunk Rod", "Fluorescent Rod"},
-            ["Ancient Ruin"] = {"Element Rod", "Ghostfinn Rod",},
+            ["Treasure Room"] = {"Element Rod", "Ghostfinn Rod",},
         },
         ["Endgame"] = "Element Rod",
     },
