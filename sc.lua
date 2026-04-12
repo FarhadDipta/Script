e649be3300e8a8bc7cd14208a9c6ae1b244bfc83
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2026-04-12 23:59:30 +0700

    1

--- sc.lua
+++ sc.lua
@@ -9,7 +9,7 @@ _G.FishItConfig = _G.FishItConfig or {
             "Sacred Guardian Squid",
             "Eggy Enchant Stone",
             "Silly Easter Duck",
-            "Velastra"
+            "Velastra",
             {Name = "Ruby", Variant = "Gemstone"},
             {Name = "Blob Fish", Variant = "Ghost"},
             {Name = "Blob Fish", Variant = "Galaxy"},
